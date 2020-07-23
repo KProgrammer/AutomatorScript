d2d6dd82e622a93af7f0136adc3789c1759d2097
Krutarth Shah <krutarthparag.74@hotmail.com>
2020-07-23 08:12:18 +0530

    Changed the structure

--- src/parser.py
+++ src/parser.py
@@ -21,30 +21,26 @@ class Command:
 
 f = open(sys.argv[1])
 
-for line in f:
-    line = line.strip()
-    lineList = line.split(' ')
-    command = lineList[0]
-    params = lineList[1:]
+commands = [
+    Command("move", functions.move, 2),
+    Command("click", functions.click, 0),
+    Command("doubleclick", functions.doubleclick, 0),
+    Command("middleclick", functions.middleclick, 0),
+    Command("rightclick", functions.rightclick, 0),
+    Command("click", functions.clickPicture, 1),
+    Command("doubleclick", functions.doubleClickPicture, 1),
+    Command("middleclick", functions.middleClickPicture, 1),
+    Command("rightclick", functions.rightClickPicture, 1),
+    Command("getinfogui", functions.getinfogui, 0),
+    Command("scroll", functions.scroll, 1),
+    Command("type", functions.typeF, 1),
+    Command("hotkey", functions.hotkey, -1),
+    Command("screenshot", functions.screenshot, 1),
+    Command("wait", functions.wait, 1)
+]
 
-    commands = [
-        Command("move", functions.move, 2),
-        Command("click", functions.click, 0),
-        Command("doubleclick", functions.doubleclick, 0),
-        Command("middleclick", functions.middleclick, 0),
-        Command("rightclick", functions.rightclick, 0),
-        Command("click", functions.clickPicture, 1),
-        Command("doubleclick", functions.doubleClickPicture, 1),
-        Command("middleclick", functions.middleClickPicture, 1),
-        Command("rightclick", functions.rightClickPicture, 1),
-        Command("getinfogui", functions.getinfogui, 0),
-        Command("scroll", functions.scroll, 1),
-        Command("type", functions.typeF, 1),
-        Command("hotkey", functions.hotkey, -1),
-        Command("screenshot", functions.screenshot, 1),
-        Command("wait", functions.wait, 1)
-    ]
 
+def run(command, params):
     for new_command in commands:
         if command == new_command.command and new_command.params_len == -1:
             new_command.run(params)
@@ -52,5 +48,14 @@ for line in f:
         elif command == new_command.command and len(params) == new_command.params_len:
             new_command.run(params)
             break
+
+
+for line in f:
+    line = line.strip()
+    lineList = line.split(' ')
+    command = lineList[0]
+    params = lineList[1:]
+
+    run(command, params)
 
 f.close()
